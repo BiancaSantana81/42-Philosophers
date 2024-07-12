63a0372450fb9d76543553d457c558b91d8476a0
bsantana <biancasantana81@hotmail.com>
2024-07-12 19:31:09 -0300

    finish: retesting

--- philo/6_clean.c
+++ philo/6_clean.c
@@ -6,7 +6,7 @@
 /*   By: bsantana <bsantana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:41:19 by bsantana          #+#    #+#             */
-/*   Updated: 2024/07/11 17:53:11 by bsantana         ###   ########.fr       */
+/*   Updated: 2024/07/12 19:29:27 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	free_everything(t_table *table)
 {
 	if (table->philo)
 		free(table->philo);
-	else if (table->fork)
+	if (table->fork)
 		free(table->fork);
-	else if (table)
+	if (table)
 		free(table);
 }
 
@@ -27,25 +27,26 @@ void	destroy_mutexes(t_table *table)
 	int	i;
 	int	j;
 
-	i = 0;
-	j = 0;
+	i = -1;
+	j = -1;
 	if (pthread_mutex_destroy(&table->print) != 0)
 		error_message("Error destroying print mutex.", table);
 	else if (pthread_mutex_destroy(&table->end_simulation_mutex) != 0)
 		error_message("Error destroying end mutex.", table);
-	while (i < table->fork->id)
+	while (++i < table->fork->id)
 	{
 		if (pthread_mutex_destroy(&table->fork[i].fork) != 0)
 			error_message("Error destroying fork mutex.", table);
-		i++;
 	}
-	while (j < table->philo_nbr)
+	while (++j < table->philo_nbr)
 	{
 		if (pthread_mutex_destroy(&table->philo[j].meals_counter_mutex) != 0)
 			error_message("Error destroying meals_counter.", table);
 		else if (pthread_mutex_destroy(
 				&table->philo[j].last_meal_time_mutex) != 0)
 			error_message("Error destroying last_meal_time.", table);
-		j++;
+		else if (pthread_mutex_destroy(
+				&table->philo[j].full_mutex) != 0)
+			error_message("Error destroying last_meal_time.", table);
 	}
 }
